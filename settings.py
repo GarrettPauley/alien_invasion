c8deef066d3a82cc7d27338da88fbe06e9486853
FlatBiped <garrettpauley18@gmail.com>
2020-08-23 20:08:23 -0400

    Setup/Ship

---
+++ settings.py
@@ -0,0 +1,10 @@
+
+class Settings: 
+    """A class to store all of the game settings"""
+
+    def __init__(self): 
+        """initialize the game's settings"""
+        self.screen_width = 800 
+        self.screen_height = 500 
+        self.bg_color = (230,230,230)
+        self.ship_speed = 1.5
